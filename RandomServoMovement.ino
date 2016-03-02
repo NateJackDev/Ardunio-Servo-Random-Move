31ba1ad2c41b8b2992243060bfb4f3e5ea053977
Nate Jackson <jacksonn18@avphs.net>
2016-03-02 10:53:07 -0800

    Edited Comments and added Signature

--- RandomServoMovement.ino
+++ RandomServoMovement.ino
@@ -1,13 +1,15 @@
-//  _ ___ _______     ___ ___ ___  ___ _   _ ___ _____ ___ 
-// / |_  )__ /   \   / __|_ _| _ \/ __| | | |_ _|_   _/ __| 
-// | |/ / |_ \ |) | | (__ | ||   / (__| |_| || |  | | \__ \ 
-// |_/___|___/___/   \___|___|_|_\\___|\___/|___| |_| |___/ 
-// 
+//  ________   ________  _________  _______         ___  ________  ________  ___  __    ________  _______   ___      ___ 
+// |\   ___  \|\   __  \|\___   ___\\  ___ \       |\  \|\   __  \|\   ____\|\  \|\  \ |\   ___ \|\  ___ \ |\  \    /  /|
+// \ \  \\ \  \ \  \|\  \|___ \  \_\ \   __/|      \ \  \ \  \|\  \ \  \___|\ \  \/  /|\ \  \_|\ \ \   __/|\ \  \  /  / /
+//  \ \  \\ \  \ \   __  \   \ \  \ \ \  \_|/__  __ \ \  \ \   __  \ \  \    \ \   ___  \ \  \ \\ \ \  \_|/_\ \  \/  / / 
+//   \ \  \\ \  \ \  \ \  \   \ \  \ \ \  \_|\ \|\  \\_\  \ \  \ \  \ \  \____\ \  \\ \  \ \  \_\\ \ \  \_|\ \ \    / /  
+//    \ \__\\ \__\ \__\ \__\   \ \__\ \ \_______\ \________\ \__\ \__\ \_______\ \__\\ \__\ \_______\ \_______\ \__/ /   
+//     \|__| \|__|\|__|\|__|    \|__|  \|_______|\|________|\|__|\|__|\|_______|\|__| \|__|\|_______|\|_______|\|__|/    
+//
 // Random Servo Movement
 // 
 // Made by Nathan Jackson
 // License: MIT
-// Downloaded from: https://123d.circuits.io/circuits/1692451-random-servo-movement
 
 #include <Servo.h> 
 
@@ -20,25 +22,25 @@ int ypos;
 
 void setup() { 
 	
-  	xservo.attach(9); 
-	yservo.attach(10);
-	zservo.attach(11);
+  	xservo.attach(9); //Attach the signal pin to pin 9 
+	yservo.attach(10); //Attach the signal pin to pin 10
+	zservo.attach(11); //Attach the signal pin to pin 11
 } 
 
 void loop() { 
-	{ 
-		ypos = random(180);  
-		yservo.write(ypos); 
-		delay(200); 
-	} 
-	{ 
-		xpos = random(180);  
-		xservo.write(xpos); 
-		delay(200); 
-	} 
-	{ 
-		ypos = random(180); 
-		zservo.write(ypos);  
-		delay(200); 
-	} 
+{ 
+	ypos = random(180); // The degree in random number selection 
+	yservo.write(ypos); 
+	delay(200); //Delay on Servo change
+} 
+{ 
+	xpos = random(180);  
+	xservo.write(xpos); 
+	delay(200); 
+} 
+{ 
+	ypos = random(180); 
+	zservo.write(ypos);  
+	delay(200); 
+} 
 } 
